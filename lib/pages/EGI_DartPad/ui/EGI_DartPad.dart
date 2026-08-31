d4b754a5eb0426fada01893c742b4f73f0151d5d
Sebastiano Saccuzzo <138576177+Sebastiano1997@users.noreply.github.com>
2026-08-31 19:49:47 +0200

    Create placeholder: pages/EGI_DartPad/ui/EGI_DartPad.dart

---
+++ lib/pages/EGI_DartPad/ui/EGI_DartPad.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EgiDartPad extends StatelessWidget {
+  const EgiDartPad({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('EGI DartPad')),
+      body: const Center(child: Text('Placeholder per EGI DartPad')),
+    );
+  }
+}
