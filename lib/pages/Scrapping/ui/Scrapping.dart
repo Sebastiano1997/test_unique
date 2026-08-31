47dcebc0e292dac749a79ede1e9d21f7e734c903
Sebastiano Saccuzzo <138576177+Sebastiano1997@users.noreply.github.com>
2026-08-31 19:50:43 +0200

    Create placeholder: pages/Scrapping/ui/Scrapping.dart

---
+++ lib/pages/Scrapping/ui/Scrapping.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ScrappingPage extends StatelessWidget {
+  const ScrappingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Scrapping')),
+      body: const Center(child: Text('Placeholder per Scrapping')),
+    );
+  }
+}
